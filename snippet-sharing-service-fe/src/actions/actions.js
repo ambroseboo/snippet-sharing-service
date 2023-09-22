bd4ec782489998568a03bea40efc55ff330e0e63
Ambrose Boo <ambroseboo@Ambroses-MacBook-Air.local>
2023-09-22 22:56:28 +0800

    Change axios redirect to correct proxy

--- snippet-sharing-service-fe/src/actions/actions.js
+++ snippet-sharing-service-fe/src/actions/actions.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
+const proxy = "https://snippet-sharing-service-backend.onrender.com";
 
 export const getSnippets = async () => {
-    return axios.get('/snippets')
+    return axios.get(proxy + '/snippets')
         .then(res => res.data)
 }
 
 export const postSnippet = async (data) => {
-    return axios.post('/snippet', data)
+    return axios.post(proxy + '/snippet', data)
         .catch((err) => {
             console.log(err)
         }).then (res => res.data)
 }
 
 export const getSnippet = async (params) => {
-    return axios.get(`/snippet/${params.url_hash}`)
+    return axios.get(`${proxy}/snippet/${params.url_hash}`)
         .then(res => res.data)
 }
