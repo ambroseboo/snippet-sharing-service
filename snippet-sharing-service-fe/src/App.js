fb179ea0fddc7c486f486415cd545502c6ef263e
Ambrose Boo <ambroseboo@Ambroses-MacBook-Air.local>
2023-09-22 18:39:17 +0800

    Add Navbar

--- snippet-sharing-service-fe/src/App.js
+++ snippet-sharing-service-fe/src/App.js
@@ -6,19 +6,23 @@ import { GetSnippets } from './components/GetSnippets.js';
 import { ViewSnippet } from './components/ViewSnippet.js';
 import { NotFound } from './components/NotFound.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { NavBar } from './components/NavBar.js'; 
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<GetSnippets />} />
-        <Route path="/snippets" element={<GetSnippets />} />
-        <Route path="/snippet" element={<CreateSnippet />} />
-        <Route path="/snippet/:url_hash" element={<ViewSnippet />} />
-        <Route path="/notfound" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <div>
+      <NavBar/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<GetSnippets />} />
+          <Route path="/snippets" element={<GetSnippets />} />
+          <Route path="/snippet" element={<CreateSnippet />} />
+          <Route path="/snippet/:url_hash" element={<ViewSnippet />} />
+          <Route path="/notfound" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
   )};
 
 export default App;
--- snippet-sharing-service-fe/src/components/NavBar.js
+++ snippet-sharing-service-fe/src/components/NavBar.js
@@ -1,3 +1,20 @@
-const NavBar = () => {
-    
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+export function NavBar() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="/">Snippet Sharing</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href=".">Home</Nav.Link>
+            <Nav.Link href="/snippet">Create Snippet</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
